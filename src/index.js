6ab8fab4050f6023f74fc97b5d48b1a362ac46f5
Daniel Whiffing <danielwhiffing@gmail.com>
2020-03-08 15:01:37 -0400

    Removes react-dnd

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import Backend from 'react-dnd-html5-backend'
-import { DndProvider } from 'react-dnd'
 
-ReactDOM.render(
-  <DndProvider backend={Backend}>
-    <App />
-  </DndProvider>,
-  document.getElementById('root'),
-)
+ReactDOM.render(<App />, document.getElementById('root'))
